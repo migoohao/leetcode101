2cd7e5838d37296597cd25ee9c310cc55021650a
migoo <migoo_hao@163.com>
2021-02-15 01:53:28 +0800

    dp 1

---
+++ chapter7/70.ClimbingStairs(Easy).java
@@ -0,0 +1,16 @@
+class Solution {
+    private static int[] dp=new int[46];
+    static {
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+    }
+    public int climbStairs(int n) {
+        if (n<=0||n>dp.length){
+            return 0;
+        }
+        return dp[n];
+    }
+}
