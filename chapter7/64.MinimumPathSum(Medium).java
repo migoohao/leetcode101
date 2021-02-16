1f3b859aa22c463456e180536e06245440ab5634
migoo <migoo_hao@163.com>
2021-02-16 22:42:41 +0800

    dp 64

--- chapter7/64.MinimumPathSum(Medium).java
+++ chapter7/64.MinimumPathSum(Medium).java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
     public int minPathSum(int[][] grid) {
-        return 0;
+        int[] dp = new int[grid[0].length + 1];
+        Arrays.fill(dp, 1, dp.length, Integer.MAX_VALUE);
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                dp[col + 1] = Math.min(dp[col], dp[col + 1]) + grid[row][col];
+            }
+            dp[0] = Integer.MAX_VALUE;
+        }
+        return dp[dp.length - 1];
     }
 }
