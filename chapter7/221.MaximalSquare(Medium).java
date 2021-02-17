277cea3a20e6ceb27c2499f146eadc96bea41359
migoo <migoo_hao@163.com>
2021-02-17 18:56:50 +0800

    format 221

--- chapter7/221.MaximalSquare(Medium).java
+++ chapter7/221.MaximalSquare(Medium).java
@@ -1,17 +1,17 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int[][] dp=new int[2][matrix[0].length+1];
-        int result=0;
-        for(int row=0;row<matrix.length;row++){
-            for(int col=0;col<matrix[row].length;col++){
-                if(matrix[row][col]=='1'){
-                    dp[(row+1)%2][col+1]=Math.min(Math.min(dp[(row)%2][col],dp[(row)%2][col+1]),dp[(row+1)%2][col])+1;
-                    result=Math.max(result,dp[(row+1)%2][col+1]);
-                }else{
-                    dp[(row+1)%2][col+1]=0;
+        int[][] dp = new int[2][matrix[0].length + 1];
+        int result = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == '1') {
+                    dp[(row + 1) % 2][col + 1] = Math.min(Math.min(dp[(row) % 2][col], dp[(row) % 2][col + 1]), dp[(row + 1) % 2][col]) + 1;
+                    result = Math.max(result, dp[(row + 1) % 2][col + 1]);
+                } else {
+                    dp[(row + 1) % 2][col + 1] = 0;
                 }
             }
         }
-        return result*result;
+        return result * result;
     }
 }
