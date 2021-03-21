12da5ff4002cf4a7eab76fdef1306edc10ab1773
migoo <migoo_hao@163.com>
2021-03-21 01:39:11 -0400

    update and complete 72

--- chapter7/72.EditDistance(Hard).java
+++ chapter7/72.EditDistance(Hard).java
@@ -3,9 +3,15 @@ class Solution {
         char[] c1 = word1.toCharArray();
         char[] c2 = word2.toCharArray();
         int[][] dp = new int[c1.length + 1][c2.length + 1];
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+        }
         for (int i = 0; i < c1.length; i++) {
             for (int j = 0; j < c2.length; j++) {
-                    int pre = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
+                int pre = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
                 if (c1[i] == c2[j]) {
                     dp[i + 1][j + 1] = Math.min(pre, dp[i][j]);
                 } else {
