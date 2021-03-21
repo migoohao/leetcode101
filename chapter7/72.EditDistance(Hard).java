3bc2f2e6e68a4954ad5e0e4b0d6787eab272042a
migoo <migoo_hao@163.com>
2021-03-21 01:37:46 -0400

    complete 72

--- chapter7/72.EditDistance(Hard).java
+++ chapter7/72.EditDistance(Hard).java
@@ -1,5 +1,18 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-
+        char[] c1 = word1.toCharArray();
+        char[] c2 = word2.toCharArray();
+        int[][] dp = new int[c1.length + 1][c2.length + 1];
+        for (int i = 0; i < c1.length; i++) {
+            for (int j = 0; j < c2.length; j++) {
+                    int pre = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
+                if (c1[i] == c2[j]) {
+                    dp[i + 1][j + 1] = Math.min(pre, dp[i][j]);
+                } else {
+                    dp[i + 1][j + 1] = Math.min(pre, dp[i][j] + 1);
+                }
+            }
+        }
+        return dp[c1.length][c2.length];
     }
 }
